e88784bbed44cc5f1d4ed08023a5e226c31ec606
Zuleyma Pelcastre Vargas <pelcastrevargasz@gmail.com>
2025-03-31 10:31:25 -0600

    24 Book property

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -3,9 +3,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .forms import PropertyForm
-
-from .models import Property
-from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
+from .models import Property, Reservation
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -27,7 +26,45 @@ def properties_detail(request, pk):
 
     serializer = PropertiesDetailSerializer(property, many=False)
 
-    return JsonResponse(serializer.data)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_reservation(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservation = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservation, many=True)
+
+    return JsonResponse(serializer.data,safe=False)
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
 
 @api_view(['POST', 'FILES'])
 def create_property(request):
